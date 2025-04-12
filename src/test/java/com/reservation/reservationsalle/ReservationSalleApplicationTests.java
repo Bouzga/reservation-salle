fb61fba58caf3b7643db67ae506f6bf9d1894673
Bouzga <bouzgaoumnia@gmail.com>
2025-04-12 23:45:40 +0100

    Initial commit

---
+++ src/test/java/com/reservation/reservationsalle/ReservationSalleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.reservationsalle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSalleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
